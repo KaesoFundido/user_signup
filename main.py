b10f6e414f750dde645e7fc531cfb7ed7fae0b65
Kaela Nichols <kae.nichols28@gmail.com>
2019-12-02 19:26:49 -0600

    complete

--- main.py
+++ main.py
@@ -1,35 +1,38 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, redirect, render_template, url_for
 import html
-
-
-
-
+import os
 
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-form = """
 
-
-
-"""
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html", username="", email="", error_type="", error_message="")
-@app.route("/", methods=['POST'])
+    return render_template('index.html', ispost = "GET")
+
+@app.route('/', methods=['POST'])
 def login():
-    if request.form["password"] =="":
-        return render_template("index.html", username= request.form['username'], email= request.form['email'], error_type="bad_password", error_message="This is an invalid password")
-    else:
-        return render_template("welcome.html")
+    if request.method =="POST":
 
-@app.route("/welcome")
+        password = request.form['password']
+        username = request.form['username']
+        email = request.form['email']
+        verify = request.form['verify']
+        ispost = request.method
+        if len(password) > 3 and len(password) < 20 and ' ' not in password:
+            if len(username) > 3 and len(username) < 20 and ' ' not in username:
+                if verify == password and verify != '':
+                    if email == '' or email.count('@') == 1 and email.count('.') == 1 and len(email) > 3 and len(email) < 20 and ' ' not in email:  
+                        return redirect(url_for('welcome'), code=307)
+        return render_template('index.html', username=username,password=password, verify=verify, email=email, ispost=ispost)
+
+ 
+
+@app.route('/welcome', methods=['POST'])
+def welcome():
+    username= request.form['username']
+    return render_template('welcome.html', username=username)
 
 
-def valid_user(username, password, validation):
-    return render.template("welcome.html")
-
-
-#hello flask and time validation code!
 app.run()
